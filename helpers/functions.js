dfa620dd1ae4dd0cb42151a7922570905e013ec6
Jayden Shaw <jayden.shaw@gmail.com>
2018-11-14 23:02:09 +0000

    Helper Functions

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,11 +1,24 @@
 module.exports = {
 
 /*Functions:
-    - Math.random (cookies)
-    - Pass Hash
-    - Check login status
-    - Check user database
-    - Rating average
+    - Math.random (cookies) * DONE *
+    - Rating average * DONE *
     - Filter results
     - Form requirements
+*/
 }
+
+// Random Number Generatior
+function randomNum() {
+    return Math.random().toString(36).substring(7)
+}
+
+// Average for Ratings
+function avRat(rating) {
+    var avR = 0, i;
+    for (i = 0; i < rating.length; i += 1) {
+        avR += rating[i];
+    }
+    return avR / rating.length;
+}
+
