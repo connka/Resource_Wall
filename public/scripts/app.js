4308214a4254d251719e1ef31f9d708a1498dd3c
Kate Connolly <katherine.d.connolly@gmail.com>
2018-11-16 18:50:36 +0000

    skeleton for data rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,42 @@
-$(() => {
+function createResourseElement(resourseData) {
+    let $data = TODO:;
+  
+    return $data;
+  }
+
+  function renderResourse(resourses) {
+    resourses.forEach(resourse => {
+      $("resourses").append(createResourseElement(resoure));
+    });
+  }
+
+// Calls on Page Load
+$("document").ready(() => {
+  function renderResourses(resourses) {
+      resourses.forEach(resourseData => {
+          $("resourseList").append(createResourseElement(resourseData));
+      });
+  }
+
+function loadResourses(callback) {
+    $.ajax({
+      method: 'GET',
+      url: '/api/resources'
+    }).done(users => {
+      for (resourse of resourses) {
+        $('<div>')
+          .text(resourse)
+          .appendTo($('body'));
+      }
+    });
+  }
+  loadResourses(renderResourses);
+});
+
+
+//Render User Data
+
+function loadUsers(callback) {
   $.ajax({
     method: 'GET',
     url: '/api/users'
@@ -9,4 +47,6 @@ $(() => {
         .appendTo($('body'));
     }
   });
+}
+loadUsers(renderUsers);
 });
