935e7fd75881f2e85445fd6c4c072f228f5c2364
Kate Connolly <katherine.d.connolly@gmail.com>
2018-11-16 21:09:06 +0000

    added logic for rendering and loading data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,84 @@
-function createResourseElement(resourseData) {
-    let $data = TODO:;
-  
-    return $data;
-  }
+const userData = {
+  "user": {
+      "id": "1",
+      "username": "@JSmith",
+      "name": "Joe Smith",
+      "email": "joesmith@gmail.com",
+      "location": "Boston",
+      "intrests": {
+          "intrest 1": "Hiking", 
+          "intrest 2": "Programming", 
+          "intrest 3": "Drawing"
+      },
+  },
 
-  function renderResourse(resourses) {
-    resourses.forEach(resourse => {
-      $("resourses").append(createResourseElement(resoure));
-    });
+  "user": {
+      "id": "2",
+      "username": "@KatCon",
+      "name": "Kate Connolly",
+      "email": "connka87@gmail.com",
+      "location": "Calgary",
+      "intrests": {
+          "intrest 1": "Hiking", 
+          "intrest 2": "Programming", 
+          "intrest 3": "Dogs"
+      }
   }
+}
+
+
+function createUserElement(userData) {
+  console.log("User Data: ", userData);
+  let $user = `<a class="name" name ="name">${userData.name}</a>
+  <a class="username" name="username">${userData.username}</a>
+  <a class="email" name="email">${userData.email}</a>
+  <a class="location" name="location">${userData.location}</ar>   
+  <a class="intrests" name="intrests">${JSON.stringify(userData.intrest_id)}</a>` 
+
+  return $user
+}
+//console.log(createUserElement(userData));
+
+function renderUsers(users) {
+  users.forEach(user => {
+    let html = createUserElement(user);
+      $("#user-profile").append(html);
+  })
+}
+
+//Load Users
+function loadUsers(callback) {
+  $.ajax({
+      method: 'GET',
+      url: '/api/users/:id/profile', 
+      success: users => {
+        console.log('were back', users)
+          callback(users);
+      }
+  });
+}
 
 // Calls on Page Load
 $("document").ready(() => {
-  function renderResourses(resourses) {
-      resourses.forEach(resourseData => {
-          $("resourseList").append(createResourseElement(resourseData));
-      });
-  }
+  loadUsers(renderUsers);
 
-function loadResourses(callback) {
-    $.ajax({
-      method: 'GET',
-      url: '/api/resources'
-    }).done(users => {
-      for (resourse of resourses) {
-        $('<div>')
-          .text(resourse)
-          .appendTo($('body'));
-      }
-    });
-  }
-  loadResourses(renderResourses);
 });
 
 
-//Render User Data
 
-function loadUsers(callback) {
-  $.ajax({
-    method: 'GET',
-    url: '/api/users'
-  }).done(users => {
-    for (user of users) {
-      $('<div>')
-        .text(user.username)
-        .appendTo($('body'));
-    }
-  });
-}
-loadUsers(renderUsers);
-});
+// //Render User Data
+// $("document").ready(() => {
+//   function loadUsers(callback) {
+//     $.ajax({
+//       method: 'GET',
+//       url: '/api/users'
+//     }).done(users => {
+//       for (user of users) {
+//         $('<div>')
+//           .text(user.username)
+//           .appendTo($('body'));
+//       }
+//     });
+//   }
+//   loadUsers(renderUsers);
+// });
