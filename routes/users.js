92b597001eb873bcbffa42826f97b685f39e61f7
Kate Connolly <katherine.d.connolly@gmail.com>
2018-11-16 23:53:07 +0000

    reloading for merge

--- routes/users.js
+++ routes/users.js
@@ -70,6 +70,31 @@ module.exports = knex => {
       });
   });
 
+  // @route GET  api/users/:id/profile
+  // @desc  get logged in users profile
+  // @access  Private
+  router.get('/:id/profile', (req, res) => {
+    const { id } = req.params;
+    console.log(req.session.user_id);
+
+    if (id == req.session.user_id) {
+      knex
+        .select('*')
+        .from('profiles')
+        .where('user_id', id)
+        .then(profile => {
+          res.status(200).render('userpage',profile);
+        })
+        .catch(err => {
+          console.log(err);
+          // res.status(404).send('Mesaage : 404 :No resourses found');
+          res.status(404).redirect(`/api/users/${id}`);
+        });
+    } else {
+      res.status(401).redirect(`/api/users/${id}`);
+    }
+  });
+
   // @route   GET api/users/:id/rate
   // @desc    gets all rated by  current user
   // @access  Private
@@ -138,10 +163,6 @@ module.exports = knex => {
         res.status(404).send('Mesaage : 404 :No resourses found');
       });
   });
-
-  // @route   GET api/users/:id/resourses
-  // @desc    return all resourses of a user
-  // @access  Private
 
   // ============= POST REQUESTS ==================== //
 
@@ -249,6 +270,34 @@ module.exports = knex => {
       });
   });
 
+  // @route   PUT  api/users/:id/profile
+  // @desc  update logged in users profile
+  // @access  Private
+  router.post('/:id/profile', (req, res) => {
+    const { id } = req.params;
+    console.log(req.session);
+    console.log(req.body, req.session.user_id);
+    //console.log('req.params:', req.params);
+    //if (id == req.session.user_id) {
+    knex('profiles')
+      .insert({
+        ...req.body,
+        user_id: id
+      })
+      .where('user_id', id)
+      .then(profile => {
+        console.log('added Profile :', profile);
+        res.status(201).send(`/api/users/${id}/profile`);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).send('Status : 400 : Bad request');
+      });
+    //} else {
+    res.status(401).send('Status : 400 : Unauthorized');
+    //}
+  });
+
   // @route   POST api/users/:user_id/resourses/:resourse_id/like
   // @desc  logged in user addes a like to a resourse
   // @access  Private
@@ -316,5 +365,167 @@ module.exports = knex => {
       });
   });
 
+  // ============= PUT REQUESTS ==================== //
+
+  // @route   PUT  api/users/:id/profile
+  // @desc  update logged in users profile
+  // @access  Private
+  router.put('/:id/profile', (req, res) => {
+    const { id } = req.params;
+    console.log(req.session);
+    console.log(req.session.user_id);
+    //console.log('req.params:', req.params);
+    //if (id == req.session.user_id) {
+    knex('profiles')
+      .update({
+        ...req.body,
+        user_id: id
+      })
+      .where('user_id', id)
+      .then(profile => {
+        console.log('added Profile :', profile);
+        res.status(201).send(`/api/users/${id}/profile`);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).send('Status : 400 : Bad request');
+      });
+    //} else {
+    // res.status(401).send('Status : 400 : Unauthorized');
+    //}
+  });
+
+  // @route   PUT  api/users/:id/resourse
+  // @desc  update logged in users profile
+  // @access  Private
+  router.put('/:user_id/resourses/:resourse_id', (req, res) => {
+    const { user_id, resourse_id } = req.params;
+    console.log('from PUT resourse:', req.session);
+    console.log('from PUT resourse:', req.session.user_id);
+    console.log('from PUT resourse:', req.body);
+    //console.log('req.params:', req.params);
+    //if (id == req.session.user_id) {
+    knex('resourses')
+      .update(req.body)
+      .where('id', resourse_id)
+      .then(resourse => {
+        console.log('Updated resourse :', resourse);
+        //res.status(201).send(`/api/users/${id}/profile`);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).send('Status : 400 : Bad request');
+      });
+    //} else {
+    // res.status(401).send('Status : 400 : Unauthorized');
+    //}
+  });
+
+  // @route   PUT  api/users/:id/resourses
+  // @desc  update logged in users profile
+  // @access  Private
+  router.put(
+    '/:user_id/resourses/:resourse_id/comment/:comment_id',
+    (req, res) => {
+      const { user_id, resourse_id, comment_id } = req.params;
+      console.log('from PUT comments:', req.session);
+      console.log('from PUT comments:', req.session.user_id);
+      console.log('from PUT comments:', req.body);
+      //console.log('req.params:', req.params);
+      //if (id == req.session.user_id) {
+      knex('user_comments')
+        .update(req.body)
+        .where({
+          id: comment_id,
+          user_id: user_id,
+          resourse_id: resourse_id
+        })
+        .then(resourse => {
+          console.log('Updated resourse :', resourse);
+          //res.status(201).send(`/api/users/${id}/profile`);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(400).send('Status : 400 : Bad request');
+        });
+      //} else {
+      // res.status(401).send('Status : 400 : Unauthorized');
+      //}
+    }
+  );
+
+  // @route   PUT  api/users/:user_id/resourses/:resourses_id/rate
+  // @desc  update rating for a resourse by a logged in  users
+  // @access  Private
+  router.put('/:user_id/resourses/:resourse_id/rate', (req, res) => {
+    const { user_id, resourse_id } = req.params;
+    console.log('from PUT comments:', req.session);
+    console.log('from PUT comments:', req.session.user_id);
+    console.log('from PUT comments:', req.body);
+    //console.log('req.params:', req.params);
+    //if (id == req.session.user_id) {
+    knex('user_resourse_rating')
+      .update(req.body)
+      .where({
+        user_id: user_id,
+        resourse_id: resourse_id
+      })
+      .then(resourse => {
+        console.log('Updated resourse :', resourse);
+        //res.status(201).send(`/api/users/${id}/profile`);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).send('Status : 400 : Bad request');
+      });
+    //} else {
+    // res.status(401).send('Status : 400 : Unauthorized');
+    //}
+  });
+
+  // ============= DELETE REQUESTS ==================== //
+
+  // @route DELETE  api/users/:user_id/resourses/:resourses_id
+  // @desc delete a resourse
+  // @access  Private
+  router.delete('/:user_id/resourses/:resourse_id', (req, res) => {
+    const { user_id, resourse_id } = req.params;
+    console.log('from PUT comments:', req.session);
+    console.log('from PUT comments:', req.session.user_id);
+    //console.log('from PUT comments:', req.params);
+    console.log('req.params:', req.params);
+    //if (id == req.session.user_id) {
+    let deletedResourse;
+    return knex
+      .transaction(function(t) {
+        return knex('user_resourses')
+          .transacting(t)
+          .where({ user_id: user_id, resourse_id: resourse_id })
+          .delete()
+          .returning('*')
+          .then(function(response) {
+            console.log('RESPONSE:', response);
+            return knex('resourses')
+              .transacting(t)
+              .where({ id: resourse_id })
+              .delete()
+              .returning('*');
+          })
+          .then(response => {
+            deletedResourse = response[0];
+            t.commit;
+          })
+          .catch(err => (console.log('err', err), t.rollback));
+      })
+      .then(function(resp) {
+        console.log('Transaction complete.');
+        res.status(200).send(deletedResourse);
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.status(400).send('Message : 400 : Bad request');
+      });
+  });
+
   return router;
-};
+}
